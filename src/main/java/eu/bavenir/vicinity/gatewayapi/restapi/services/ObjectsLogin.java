6fe67b0719a6cae519d176bf0a6111ad21d96aea
Martin Horniak <martin.horniak@bavenir.eu>
2017-05-29 14:02:37 +0200

    Maven configuration modified to produce nice builds from terminal. Git repository cleaned of unnecessary forgotten stuff (log files, previous builds, etc.).

--- src/main/java/eu/bavenir/vicinity/gatewayapi/restapi/services/ObjectsLogin.java
+++ src/main/java/eu/bavenir/vicinity/gatewayapi/restapi/services/ObjectsLogin.java
@@ -3,13 +3,51 @@ package eu.bavenir.vicinity.gatewayapi.restapi.services;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-// TODO
+/*
+ * STRUCTURE
+ * - constants
+ * - public methods overriding HTTP methods 
+ * - private methods
+ */
+
+/**
+ * This class implements login service for objects, that are trying to authenticate against the XMPP network. Although
+ * it is not really necessary to have a separate login service while utilizing HTTP Authentication Schemes,
+ * (since the gateway automatically logs in an object that makes a call and has not been logged in yet), it can be
+ * useful for devices that only need to listen (i.e. they don't need to use any of the services, they just need to
+ * be reachable). The service does actually nothing, all the authentication stuff is happening in the 
+ * {@link eu.bavenir.vicinity.gatewayapi.restapi.security.AuthenticationVerifier AuthenticationVerifier} that is called
+ * as a RESTLET guard in the API router.
+ * 
+ *   URL: 				[server]:[port]/api/objects/login
+ *   METHODS: 			GET
+ *   SPECIFICATION:		@see <a href="https://app.swaggerhub.com/apis/fserena/vicinity_gateway_api/">Gateway API</a>
+ *   
+ * @author sulfo
+ *
+ */
 public class ObjectsLogin extends ServerResource{
 
+	// === CONSTANTS ===
 	
+	// none
+	
+	
+	// === OVERRIDEN HTTP METHODS ===
+	
+	/**
+	 * Answers the GET call, after it is authenticated.
+	 * 
+	 * @return A String indicating successful login.
+	 */
 	@Get
 	public String represent() {
 		
-		return "Login successfull.";
+		// if the login is unsuccessful, the execution will never reach this place
+		return "Login successful.";
 	}
+	
+	// === PRIVATE METHODS ===
+	
+	// none
 }
--- src/main/java/eu/bavenir/vicinity/gatewayapi/restapi/services/ObjectsLogout.java
+++ src/main/java/eu/bavenir/vicinity/gatewayapi/restapi/services/ObjectsLogout.java
@@ -1,15 +1,48 @@
 package eu.bavenir.vicinity.gatewayapi.restapi.services;
 
-import org.restlet.representation.Representation;
-import org.restlet.resource.ResourceException;
+import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-// TODO
+
+/*
+ * STRUCTURE
+ * - constants
+ * - public methods overriding HTTP methods 
+ * - private methods
+ */
+
+/**
+ * This class implements logout service for object, that needs to log out of the XMPP network. Usually, while 
+ * utilizing HTTP Authentication Schemes, it is not necessary to log out, since the RESTLET provides state-less 
+ * services. However, since the Gateway API works as a translator between state-less API and the state-full XMPP 
+ * network, it is necessary to include such a service to log out of the network. 
+ * 
+ *   URL: 				[server]:[port]/api/objects/logout
+ *   METHODS: 			GET
+ *   SPECIFICATION:		@see <a href="https://app.swaggerhub.com/apis/fserena/vicinity_gateway_api/">Gateway API</a>
+ *   
+ * @author sulfo
+ *
+ */
 public class ObjectsLogout extends ServerResource{
 
-	@Override
-	protected Representation get() throws ResourceException {
-		// TODO Auto-generated method stub
-		return super.get();
+	// === CONSTANTS ===
+	
+	// none
+	
+	
+	// === OVERRIDEN HTTP METHODS ===
+	
+	@Get
+	public String represent() {
+		
+		// TODO
+		return "Logout successful.";
 	}
+	
+	
+	// === PRIVATE METHODS ===
+	
+	// none
+	
 }
