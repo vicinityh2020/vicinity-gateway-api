05667f801cfc45bc85ffa8d9ccd13006a65e1f2a
Andrej Šebeň <46315186+AndrejSeben@users.noreply.github.com>
2019-04-02 11:04:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,10 @@ docker rm gtw-api >/dev/null 2>&1
 docker rmi gtw-api >/dev/null 2>&1
 docker build -f Dockerfile.gateway -t gtw-api .
 
+# CREATE FOLDERS
+mkdir -p ~/docker_data
+mkdir -p ~/docker_data/logs
+mkdir -p ~/docker_data/logs/gateway
+
 # RUN GATEWAY
 docker run -d -p 8181:8181 -v ~/docker_data/logs/gateway:/gateway/log -it --rm --name gtw-api gtw-api:latest
